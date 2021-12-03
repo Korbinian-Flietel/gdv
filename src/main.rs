be73c505204416f07275f13261bf9d53b88d523f
korbinian <korbinianfl98@googlemail.com>
2021-12-03 15:40:09 +0100

    only api left

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![feature(decl_macro)]
-#[macro_use] 
+#[macro_use]
 extern crate rocket;
 
 use test_mango::*;
-
 
 #[get("/")]
 fn index() -> &'static str {
@@ -14,15 +13,12 @@ fn index() -> &'static str {
 fn get_mvv_data(typ: String, from: Option<String>, to: Option<String>) -> String {
     let v = get_data(&typ, from, to);
     match v {
-        Some(t) =>  { 
-            serde_json::to_string(&t).unwrap()
-        },
-        None => format!("No such air property named: \"{}\" in mvv Data!", typ)
+        Some(t) => serde_json::to_string(&t).unwrap(),
+        None => format!("No such air property named: \"{}\" in mvv Data!", typ),
     }
 }
 
 #[launch]
 fn rocket() -> _ {
-    std::thread::spawn(|| transfer_mvv_data());
     rocket::build().mount("/", routes![index, get_mvv_data])
 }
